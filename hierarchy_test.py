02839a74c5a92de01775b72ecc7256e9d40f3518
Evan <evanmschultz@gmail.com>
2023-11-30 21:12:34 -0700

    Continues refactoring test

--- hierarchy_test.py
+++ hierarchy_test.py
@@ -1,101 +1,19 @@
-from typing import Any, Callable, Protocol, Union
-import libcst as cst
+from abc import ABC, abstractmethod
+import json
+from typing import Any, Callable, Union
+
+import libcst
 from libcst._nodes.internal import CodegenState
 
-import json
-from abc import ABC, abstractmethod
+from id_generation.id_generation_strategies import (
+    ClassIDGenerationStrategy,
+    FunctionIDGenerationStrategy,
+    ModuleIDGenerationStrategy,
+)
 
 from models.enums import BlockType
 
 
-# ID Generation Strategy Classes
-class IDGenerationStrategy(ABC):
-    """Abstract base class for ID generation strategies."""
-
-    @staticmethod
-    @abstractmethod
-    def generate_id(**kwargs) -> str:
-        """Generate an ID based on the given context."""
-        pass
-
-
-class ModuleIDGenerationStrategy(IDGenerationStrategy):
-    """ID generation strategy for modules."""
-
-    @staticmethod
-    def generate_id(file_path: str) -> str:
-        return f"{file_path}__>__MODULE"
-
-
-class ClassIDGenerationStrategy(IDGenerationStrategy):
-    """ID generation strategy for classes."""
-
-    @staticmethod
-    def generate_id(parent_id: str, class_name: str) -> str:
-        return f"{parent_id}__>__CLASS__{class_name}"
-
-
-class FunctionIDGenerationStrategy(IDGenerationStrategy):
-    """ID generation strategy for functions."""
-
-    @staticmethod
-    def generate_id(parent_id: str, function_name: str) -> str:
-        return f"{parent_id}__>__FUNCTION__{function_name}"
-
-
-class StandaloneCodeBlockIDGenerationStrategy(IDGenerationStrategy):
-    """ID generation strategy for standalone code blocks."""
-
-    @staticmethod
-    def generate_id(parent_id: str) -> str:
-        return f"{parent_id}__>__STANDALONE_CODE_BLOCK"
-
-
-# Node classes with a reference to a builder (Bridge Pattern)
-class BaseNode:
-    def __init__(self, name, node_type, builder, node_id):
-        self.name = name
-        self.type = node_type
-        self.id = node_id
-        self.builder = builder
-        self.children = []
-        self.comments = []  # Storing comments
-        self.params = {}  # Storing parameters
-
-    def add_child(self, child):
-        self.children.append(child)
-        self.builder.add_information(child.name, child.type)
-
-    def add_comment(self, comment):
-        self.comments.append(comment)
-
-    def add_param(self, param_name, param_info):
-        self.params[param_name] = param_info
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "type": self.type,
-            "children": [child.to_dict() for child in self.children],
-            "comments": self.comments,
-            "params": self.params,
-        }
-
-
-class ModuleNode(BaseNode):
-    pass
-
-
-class ClassNode(BaseNode):
-    pass
-
-
-class FunctionNode(BaseNode):
-    pass
-
-
-# Builder Classes (Simplified for this example)
 class BaseModelBuilder:
     def add_information(self, name, node_type):
         pass
@@ -116,54 +34,131 @@ class ModuleModelBuilder(BaseModelBuilder):
     pass
 
 
-NodeType = Union[ModuleNode, ClassNode, FunctionNode]
+ModelBuilderType = Union[ModuleModelBuilder, ClassModelBuilder, FunctionModelBuilder]
 
 
-# Factory for creating nodes
-class NodeFactory:
-    # Strategy map for node creation
-    _creation_strategies: dict[str, Callable[..., Any]] = {
-        "module": lambda name, parent_id=None: ModuleNode(
-            name=name,
-            node_type="module",
-            builder=ModuleModelBuilder(),
-            node_id=ModuleIDGenerationStrategy.generate_id(file_path=name),
+class BaseBridge(ABC):
+    def __init__(self, id: str) -> None:
+        self.id: str = id
+        self.children: list[BridgeType] = []
+        self.comments: list[str] = []
+
+        self.block_type: BlockType
+        self.builder: ModelBuilderType
+
+    def add_child(self, child) -> None:
+        self.children.append(child)
+
+    def add_comment(self, comment):
+        self.comments.append(comment)
+
+    @abstractmethod
+    def call_build_model(self) -> dict[str, str | list | dict]:
+        ...
+
+
+class ModuleBridge(BaseBridge):
+    def __init__(self, id: str) -> None:
+        super().__init__(id)
+
+        self.block_type = BlockType.MODULE
+        self.builder = ModuleModelBuilder()
+
+    def call_build_model(self) -> dict[str, str | list | dict]:
+        return {
+            "id": self.id,
+            "block_type": self.block_type.name,
+            "children": [child.call_build_model() for child in self.children],
+            "comments": self.comments,
+        }
+
+
+class ClassBridge(BaseBridge):
+    def __init__(self, id: str, class_name: str) -> None:
+        super().__init__(id)
+
+        self.class_name: str = class_name
+        self.block_type = BlockType.CLASS
+        self.builder = ClassModelBuilder()
+
+    def call_build_model(self) -> dict[str, str | list | dict]:
+        return {
+            "id": self.id,
+            "class_name": self.class_name,
+            "block_type": self.block_type.name,
+            "children": [child.call_build_model() for child in self.children],
+            "comments": self.comments,
+        }
+
+
+class FunctionBridge(BaseBridge):
+    def __init__(self, id: str, function_name: str) -> None:
+        super().__init__(id)
+
+        self.function_name: str = function_name
+        self.params: dict[str, str] = {}
+        self.block_type = BlockType.FUNCTION
+        self.builder = FunctionModelBuilder()
+
+    def add_param(self, param_name, param_info) -> None:
+        self.params[param_name] = param_info
+
+    def call_build_model(self) -> dict[str, str | list | dict]:
+        return {
+            "id": self.id,
+            "function_name": self.function_name,
+            "block_type": self.block_type.name,
+            "children": [child.call_build_model() for child in self.children],
+            "comments": self.comments,
+            "params": self.params,
+        }
+
+
+BridgeType = Union[ModuleBridge, ClassBridge, FunctionBridge]
+
+
+class BridgeFactory:
+    _creation_strategies: dict[BlockType, Callable[..., Any]] = {
+        BlockType.MODULE: lambda file_path, name=None, parent_id=None: ModuleBridge(
+            id=ModuleIDGenerationStrategy.generate_id(file_path=file_path),
         ),
-        "class": lambda name, parent_id: ClassNode(
-            name=name,
-            node_type="class",
-            builder=ClassModelBuilder(),
-            node_id=ClassIDGenerationStrategy.generate_id(
+        BlockType.CLASS: lambda name, parent_id, file_path=None: ClassBridge(
+            class_name=name,
+            id=ClassIDGenerationStrategy.generate_id(
                 parent_id=parent_id, class_name=name
             ),
         ),
-        "function": lambda name, parent_id: FunctionNode(
-            name=name,
-            node_type="function",
-            builder=FunctionModelBuilder(),
-            node_id=FunctionIDGenerationStrategy.generate_id(
+        BlockType.FUNCTION: lambda name, parent_id, file_path=None: FunctionBridge(
+            function_name=name,
+            id=FunctionIDGenerationStrategy.generate_id(
                 parent_id=parent_id, function_name=name
             ),
         ),
     }
 
     @staticmethod
-    def create_node(node_type, name, parent_id=None) -> NodeType:
-        if node_type not in NodeFactory._creation_strategies:
-            raise ValueError(f"Unknown node type: {node_type}")
-        return NodeFactory._creation_strategies[node_type](name, parent_id=parent_id)
+    def create_bridge_instance(
+        *,
+        block_type: BlockType,
+        name: str | None = None,
+        parent_id: str | None = None,
+        file_path: str | None = None,
+    ) -> Union[ModuleBridge, ClassBridge, FunctionBridge]:
+        if block_type not in BridgeFactory._creation_strategies:
+            raise ValueError(f"Unknown node type: {block_type}")
+        return BridgeFactory._creation_strategies[block_type](
+            name=name, parent_id=parent_id, file_path=file_path
+        )
 
 
-# Base Visitor
-class BaseVisitor(cst.CSTVisitor):
-    def __init__(self, file_path: str, id: str):
-        self.file_path: str = file_path
+class BaseVisitor(libcst.CSTVisitor):
+    def __init__(self, id: str) -> None:
         self.id: str = id
-        self.stack = [NodeFactory.create_node("module", file_path)]
+        self.stack: list[BridgeType] = []
 
     def extract_code_content(
         self,
-        node: cst.CSTNode,
+        node: libcst.CSTNode,
     ) -> str:
         state = CodegenState(default_indent="    ", default_newline="\n")
         node._codegen(state=state)
@@ -171,63 +166,80 @@ class BaseVisitor(cst.CSTVisitor):
         return "".join(state.tokens)
 
 
-# ModuleVisitor
 class ModuleVisitor(BaseVisitor):
-    def visit_ClassDef(self, node: cst.ClassDef):
-        class_node = NodeFactory.create_node(
-            "class", node.name.value, parent_id=self.stack[-1].id
-        )
-        self.stack[-1].add_child(class_node)
-        self.stack.append(class_node)
+    def __init__(self, id: str, module_bridge: ModuleBridge) -> None:
+        super().__init__(id)
+        self.module_bridge: ModuleBridge = module_bridge
+        self.stack.append(module_bridge)
 
-    def leave_ClassDef(self, original_node: cst.ClassDef):
+    def visit_ClassDef(self, node: libcst.ClassDef) -> None:
+        class_bridge: ClassBridge = BridgeFactory.create_bridge_instance(
+            block_type=BlockType.CLASS,
+            name=node.name.value,
+            parent_id=self.stack[-1].id,
+        )  # type: ignore
+        self.stack[-1].add_child(class_bridge)
+        self.stack.append(class_bridge)
+
+    def leave_ClassDef(self, original_node: libcst.ClassDef) -> None:
         self.stack.pop()
 
-    def visit_FunctionDef(self, node: cst.FunctionDef):
-        func_node = NodeFactory.create_node(
-            "function", node.name.value, parent_id=self.stack[-1].id
-        )
-        self.stack[-1].add_child(func_node)
-        self.stack.append(func_node)
+    def visit_FunctionDef(self, node: libcst.FunctionDef) -> None:
+        func_bridge: FunctionBridge = BridgeFactory.create_bridge_instance(
+            block_type=BlockType.FUNCTION,
+            name=node.name.value,
+            parent_id=self.stack[-1].id,
+        )  # type: ignore
+        self.stack[-1].add_child(func_bridge)
+        self.stack.append(func_bridge)
 
-    def leave_FunctionDef(self, original_node: cst.FunctionDef):
+    def leave_FunctionDef(self, original_node: libcst.FunctionDef) -> None:
         self.stack.pop()
 
-    def visit_Comment(self, node: cst.Comment):
+    def visit_Comment(self, node: libcst.Comment) -> None:
         parent_node = self.stack[-1]
         content = self.extract_code_content(node)
         parent_node.add_comment(content)
 
-    def visit_Parameters(self, node: cst.Param):
-        parent_node = self.stack[-1]
-        param_info = self.extract_code_content(node)
-        parent_node.add_param("parameters", param_info)
+    def visit_Parameters(self, node: libcst.Param) -> None:
+        parent_node_bridge = self.stack[-1]
+
+        if isinstance(parent_node_bridge, FunctionBridge):
+            param_info: str = self.extract_code_content(node)
+            parent_node_bridge.add_param("parameters", param_info)
 
 
 # Parse and Print Hierarchy Function
-def parse_and_print_hierarchy(file_path, code):
-    tree = cst.parse_module(code)
-    module_id: str = ModuleIDGenerationStrategy.generate_id(file_path=file_path)
-    visitor = ModuleVisitor(file_path=file_path, id=module_id)
+def parse_and_print_hierarchy(file_path, code) -> None:
+    tree: libcst.Module = libcst.parse_module(code)
+    module_bridge: ModuleBridge = BridgeFactory.create_bridge_instance(
+        block_type=BlockType.MODULE, file_path=file_path
+    )  # type: ignore
+
+    module_id: str = module_bridge.id
+    visitor = ModuleVisitor(id=module_id, module_bridge=module_bridge)
     tree.visit(visitor)
-    hierarchy = visitor.stack[0]
-    print(json.dumps(hierarchy.to_dict(), indent=4))
+    hierarchy = visitor.stack[0]  # NOTE: This is the stack referred to in the TODO
+    print(json.dumps(hierarchy.call_build_model(), indent=4))
 
 
 # Example usage
 code = """
 class MyClass:
-    def method1(self):
+    def method1():
         pass
 
     class NestedClass:
-        def nested_method(self):
+        def nested_method(self, param1, param2: int, param3: str = "default"):
             # This is a comment
             ...
-            
-class MyClass2:
-    def method2(self, param1, param2: int, param3: str = "default"):
-        pass
 """
 
-parse_and_print_hierarchy("./", code)
+parse_and_print_hierarchy("./ex_path", code)
+
+# TODO: Implement the list comprehension logic in the base builder class
+# TODO: Add BridgeFactory to the project
+# TODO: Update the BaseVisitor class to use the BridgeFactory and stack
+# TODO: Update the ModuleVisitor class to use the BridgeFactory
+# TODO: Update the module visitor to have all the logic from the old visitor classes
+# TODO: Update the parser to call the stack
