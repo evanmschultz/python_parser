a6c65ec5ccb36dbece5ff675f1e7d07fbb3f5225
Evan <evanmschultz@gmail.com>
2023-12-03 16:46:57 -0700

    Moves BaseVisitor class to its own file

--- visitors/base_code_block_visitor.py
+++ visitors/base_code_block_visitor.py
@@ -1,82 +1,53 @@
-# TODO: get and set dependencies logic
+from typing import Union
 
-from __future__ import annotations
-
-from abc import ABC
-from typing import TYPE_CHECKING, Union
 import libcst
+from libcst.metadata import (
+    WhitespaceInclusivePositionProvider,
+    CodeRange,
+)
+from libcst._metadata_dependent import _UNDEFINED_DEFAULT
+
+from model_builders.class_model_builder import ClassModelBuilder
+from model_builders.function_model_builder import FunctionModelBuilder
+from model_builders.module_model_builder import ModuleModelBuilder
+
+from models.models import (
+    CommentModel,
+)
+from visitors.node_processing.common_functions import (
+    extract_important_comment,
+)
+from visitors.node_processing.processing_context import PositionData
 
 
-if TYPE_CHECKING:
-    from visitors.class_def_visitor import ClassDefVisitor
-    from visitors.function_def_visitor import FunctionDefVisitor
-    from visitors.module_visitor import ModuleVisitor
+BuilderType = Union[ModuleModelBuilder, ClassModelBuilder, FunctionModelBuilder]
 
-    from model_builders.class_model_builder import ClassModelBuilder
-    from model_builders.function_model_builder import FunctionModelBuilder
-    from model_builders.module_model_builder import ModuleModelBuilder
-    from model_builders.standalone_code_block_model_builder import (
-        StandaloneCodeBlockModelBuilder,
+
+class BaseVisitor(libcst.CSTVisitor):
+    METADATA_DEPENDENCIES: tuple[type[WhitespaceInclusivePositionProvider]] = (
+        WhitespaceInclusivePositionProvider,
     )
 
-    from models.models import (
-        ClassModel,
-        FunctionModel,
-        StandaloneCodeBlockModel,
-    )
+    def __init__(self, id: str) -> None:
+        self.id: str = id
+        self.builder_stack: list[BuilderType] = []
 
+    def visit_Comment(self, node: libcst.Comment) -> None:
+        parent_builder = self.builder_stack[-1]
+        content: CommentModel | None = extract_important_comment(node)
+        if content:
+            parent_builder.add_important_comment(content)
 
-class BaseCodeBlockVisitor(libcst.CSTVisitor, ABC):
-    """
-    Abstract base class for all code block visitors.
-
-    This class provides common functionalities for all visitors, such as integration with VisitorManager
-    to track processed nodes.
-
-    Attributes:
-        model_builder (BaseModelBuilder): Builder for the code block model.
-        id (str): Identifier of the visitor.
-        parent_visitor_instance (BaseCodeBlockVisitor): Reference to the parent visitor instance.
-        children (list[BaseCodeBlockModel]): List of child models built by this visitor.
-    """
-
-    def __init__(
+    def get_node_position_data(
         self,
-        model_builder: Union[
-            "ClassModelBuilder",
-            "FunctionModelBuilder",
-            "ModuleModelBuilder",
-            "StandaloneCodeBlockModelBuilder",
-        ],
-        model_id: str,
-        parent_visitor_instance: Union[
-            "ClassDefVisitor", "FunctionDefVisitor", "ModuleVisitor", None
-        ] = None,
-    ) -> None:
-        self.model_builder: Union[
-            "ClassModelBuilder",
-            "FunctionModelBuilder",
-            "ModuleModelBuilder",
-            "StandaloneCodeBlockModelBuilder",
-        ] = model_builder
-        self.model_id: str = model_id
-        self.parent_visitor_instance: ClassDefVisitor | FunctionDefVisitor | ModuleVisitor | None = (
-            parent_visitor_instance
+        node: libcst.CSTNode,
+    ) -> PositionData:
+        position_data: CodeRange | type[_UNDEFINED_DEFAULT] = self.get_metadata(
+            WhitespaceInclusivePositionProvider, node
         )
-        self.children: list[ClassModel | FunctionModel | StandaloneCodeBlockModel] = []
-        self.children_ids_set: set[str] = set()
 
-    def not_added_to_parent_visitor(self, child_id: str) -> bool:
-        if self.parent_visitor_instance:
-            return child_id not in self.parent_visitor_instance.children_ids_set
-        else:
-            raise Exception("Parent visitor instance not set.")
-
-    def add_built_model_to_parent_visitor(
-        self, built_model: ClassModel | FunctionModel | StandaloneCodeBlockModel
-    ) -> None:
-        if self.parent_visitor_instance:
-            self.parent_visitor_instance.children.append(built_model)
-
-            if built_model.id:
-                self.parent_visitor_instance.children_ids_set.add(built_model.id)
+        start, end = 0, 0
+        if isinstance(position_data, CodeRange):
+            start: int = position_data.start.line
+            end: int = position_data.end.line
+        return PositionData(start=start, end=end)
--- visitors/module_visitor.py
+++ visitors/module_visitor.py
@@ -1,12 +1,6 @@
 from typing import Union
 
 import libcst
-from libcst.metadata import (
-    WhitespaceInclusivePositionProvider,
-    CodeRange,
-)
-from libcst._metadata_dependent import _UNDEFINED_DEFAULT
-
 
 from id_generation.id_generation_strategies import (
     ClassIDGenerationStrategy,
@@ -19,14 +13,13 @@ from model_builders.module_model_builder import ModuleModelBuilder
 
 from models.enums import BlockType
 from models.models import (
-    CommentModel,
     DecoratorModel,
     ImportModel,
     ParameterListModel,
 )
+from visitors.base_code_block_visitor import BaseVisitor
 from visitors.node_processing.common_functions import (
     extract_code_content,
-    extract_important_comment,
 )
 from visitors.node_processing.function_def_functions import (
     process_func_def,
@@ -41,36 +34,6 @@ from visitors.node_processing.processing_context import PositionData
 
 
 BuilderType = Union[ModuleModelBuilder, ClassModelBuilder, FunctionModelBuilder]
-
-
-class BaseVisitor(libcst.CSTVisitor):
-    METADATA_DEPENDENCIES: tuple[type[WhitespaceInclusivePositionProvider]] = (
-        WhitespaceInclusivePositionProvider,
-    )
-
-    def __init__(self, id: str) -> None:
-        self.id: str = id
-        self.builder_stack: list[BuilderType] = []
-
-    def visit_Comment(self, node: libcst.Comment) -> None:
-        parent_builder = self.builder_stack[-1]
-        content: CommentModel | None = extract_important_comment(node)
-        if content:
-            parent_builder.add_important_comment(content)
-
-    def get_node_position_data(
-        self,
-        node: libcst.CSTNode,
-    ) -> PositionData:
-        position_data: CodeRange | type[_UNDEFINED_DEFAULT] = self.get_metadata(
-            WhitespaceInclusivePositionProvider, node
-        )
-
-        start, end = 0, 0
-        if isinstance(position_data, CodeRange):
-            start: int = position_data.start.line
-            end: int = position_data.end.line
-        return PositionData(start=start, end=end)
 
 
 class ModuleVisitor(BaseVisitor):
