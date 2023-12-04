aa59f06ae806601090268c3b2844cdf044f2c67b
Evan <evanmschultz@gmail.com>
2023-12-04 16:51:23 -0700

    Adds StandaloneCodeBlockBuilder logic

--- model_builders/builder_factory.py
+++ model_builders/builder_factory.py
@@ -7,6 +7,7 @@ from logger.decorators import logging_decorator
 from model_builders.class_model_builder import ClassModelBuilder
 from model_builders.function_model_builder import FunctionModelBuilder
 from model_builders.module_model_builder import ModuleModelBuilder
+from model_builders.standalone_block_model_builder import StandaloneBlockModelBuilder
 
 from models.enums import BlockType
 
@@ -25,6 +26,10 @@ class BuilderFactory:
         BlockType.FUNCTION: lambda id, name, parent_id, file_path: FunctionModelBuilder(
             id=id,
             function_name=name,
+            parent_id=parent_id,
+        ),
+        BlockType.STANDALONE_CODE_BLOCK: lambda id, parent_id, name, file_path: StandaloneBlockModelBuilder(
+            id=id,
             parent_id=parent_id,
         ),
     }
@@ -61,6 +66,16 @@ class BuilderFactory:
     ) -> FunctionModelBuilder:
         ...
 
+    @staticmethod
+    @overload
+    def create_builder_instance(
+        block_type: Literal[BlockType.STANDALONE_CODE_BLOCK],
+        *,
+        id: str,
+        parent_id: str,
+    ) -> StandaloneBlockModelBuilder:
+        ...
+
     @logging_decorator(level=logging.DEBUG)
     @staticmethod
     def create_builder_instance(
@@ -70,7 +85,12 @@ class BuilderFactory:
         name: str | None = None,
         parent_id: str | None = None,
         file_path: str | None = None,
-    ) -> ModuleModelBuilder | ClassModelBuilder | FunctionModelBuilder:
+    ) -> (
+        ModuleModelBuilder
+        | ClassModelBuilder
+        | FunctionModelBuilder
+        | StandaloneBlockModelBuilder
+    ):
         if block_type not in BuilderFactory._creation_strategies:
             raise ValueError(f"Unknown node type: {block_type}")
         return BuilderFactory._creation_strategies[block_type](
