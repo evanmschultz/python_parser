f5a43fbaab73972f88f0e378bdd4cc7d5ad78b3d
Evan <evanmschultz@gmail.com>
2023-12-04 18:23:02 -0700

    Adds `NodeAndPositionData`

--- visitors/node_processing/processing_context.py
+++ visitors/node_processing/processing_context.py
@@ -1,7 +1,16 @@
 from dataclasses import dataclass
 
+import libcst
+
 
 @dataclass
 class PositionData:
+    start: int
+    end: int
+
+
+@dataclass
+class NodeAndPositionData:
+    nodes: list[libcst.CSTNode]
     start: int
     end: int
