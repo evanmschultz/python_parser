2e6bb0bfec215686a210733669e7aa29d49912a6
Evan <evanmschultz@gmail.com>
2023-12-05 12:12:54 -0700

    Moves processing_context module to utilities directory

--- utilities/processing_context.py
+++ utilities/processing_context.py
@@ -14,3 +14,10 @@ class NodeAndPositionData:
     nodes: list[libcst.CSTNode]
     start: int
     end: int
+
+
+@dataclass
+class LoggingCallerInfo:
+    caller_module_name: str
+    caller_file_path: str
+    caller_line_no: int
