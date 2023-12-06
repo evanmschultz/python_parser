b57ca0f7fbe03e8a0f03c4c8c23347a1578d565b
Evan <evanmschultz@gmail.com>
2023-12-05 18:52:28 -0700

    Adds ability to specify a custom name for the directory module map JSON file in the `save_visited_directories` method

--- visitor_manager/visitor_manager.py
+++ visitor_manager/visitor_manager.py
@@ -43,12 +43,15 @@ class VisitorManager:
             >>> vm.process_files()
             # Processes all Python files and saves their parsed data.
         """
+
         python_files: list[str] = self._get_python_files()
         for file in python_files:
             self._process_file(file)
 
     @logging_decorator(message="Saving visited directories")
-    def save_visited_directories(self) -> None:
+    def save_visited_directories(
+        self, directory_mape_name: str = "00_directory_module_map.json"
+    ) -> None:
         """
         Saves a JSON file mapping each visited directory to its Python files.
 
@@ -58,14 +61,18 @@ class VisitorManager:
             >>> vm.save_visited_directories()
             # Saves a mapping of directories to Python files as JSON.
         """
-        output_path: str = self._get_directory_map_output_path()
+
+        output_path: str = self._get_directory_map_output_path(directory_mape_name)
         self._write_json_directory_map(output_path)
 
     def _create_output_directory(self) -> None:
         """Creates the output directory if it does not already exist."""
+
         os.makedirs(self.output_directory, exist_ok=True)
 
     def _walk_directories(self) -> list[str]:
+        """Walks the specified directory and returns a list of all files."""
+
         all_files: list[str] = []
         for root, directories, files in os.walk(self.directory):
             directories[:] = [
@@ -78,16 +85,19 @@ class VisitorManager:
 
     def _filter_python_files(self, files: list[str]) -> list[str]:
         """Filters a list of files to only include Python files."""
+
         return [file for file in files if file.endswith(".py")]
 
     @logging_decorator(message="Getting Python files")
     def _get_python_files(self) -> list[str]:
         """Gets all Python files in the specified directory."""
+
         all_files: list[str] = self._walk_directories()
         return self._filter_python_files(all_files)
 
     def _process_file(self, file_path: str) -> None:
         """Processes a single Python file."""
+
         root: str = os.path.dirname(file_path)
         self.directory_modules.setdefault(root, []).append(os.path.basename(file_path))
         self._parse_and_save_file(file_path)
@@ -95,6 +105,7 @@ class VisitorManager:
     @logging_decorator(message="Processing file")
     def _parse_and_save_file(self, file_path: str) -> None:
         """Parses a Python file and saves the parsed data as JSON."""
+
         parser = PythonParser(file_path)
         code: str = parser.open_file()
         module_model: ModuleModel | None = parser.parse(code)
@@ -104,33 +115,39 @@ class VisitorManager:
     @logging_decorator(message="Saving model as JSON")
     def _save_model_as_json(self, module_model: ModuleModel, file_path: str) -> None:
         """Saves a parsed ModuleModel as JSON."""
+
         json_output_directory: str = self._create_json_output_directory()
         output_path: str = self._get_json_output_path(file_path, json_output_directory)
         self._write_json_file(module_model, output_path)
 
     def _create_json_output_directory(self) -> str:
         """Creates the JSON output directory if it does not already exist."""
+
         json_output_directory: str = os.path.join(self.output_directory, "json")
         os.makedirs(json_output_directory, exist_ok=True)
         return json_output_directory
 
     def _get_json_output_path(self, file_path: str, json_output_directory: str) -> str:
         """Gets the output path for a JSON file."""
+
         relative_path: str = os.path.relpath(file_path, self.directory)
         safe_relative_path: str = relative_path.replace(os.sep, ":").rstrip(".py")
         return os.path.join(json_output_directory, f"{safe_relative_path}.json")
 
     def _write_json_file(self, module_model: ModuleModel, output_path: str) -> None:
         """Writes a JSON file containing the parsed data from a ModuleModel."""
+
         parsed_data_json: str = module_model.model_dump_json(indent=4)
         with open(output_path, "w") as json_file:
             json_file.write(parsed_data_json)
 
-    def _get_directory_map_output_path(self) -> str:
+    def _get_directory_map_output_path(self, directory_output_name: str) -> str:
         """Gets the output path for the directory map JSON file."""
-        return os.path.join(self.output_directory, "00_directory_module_map.json")
+
+        return os.path.join(self.output_directory, directory_output_name)
 
     def _write_json_directory_map(self, output_path: str) -> None:
         """Writes the directory map JSON file."""
+
         with open(output_path, "w") as json_file:
             json.dump(self.directory_modules, json_file, indent=4)
